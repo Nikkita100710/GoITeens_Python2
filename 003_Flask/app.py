a90a2c9ecbca07ac1de7a41056424a42310e3820
Leonid Kadantsev <leonid.cordon@gmail.com>
2024-03-29 15:41:56 +0200

    commit message 005_Flask

--- 003_Flask/app.py
+++ 003_Flask/app.py
@@ -4,18 +4,23 @@ from dotenv import load_dotenv
 from enums import MAX_SCORE, NAME, students
 
 load_dotenv()
-
 app = Flask(__name__)
+
+
 # app = Flask(__name__, template_folder='template', static_folder='static')
-
-
 @app.route("/")
-def main():
-    return render_template('index.html')
+def index_():
+    # return render_template('index.html')
+    return render_template('base.html', title="Python course")
+
+
+# @app.route("/base")
+# def base_():
+#     return render_template('base.html', title="Python course")
 
 
 @app.route("/context")
-def context():
+def context_():
     context_dict = {
         "title": "Python Course",
         "name": NAME,
@@ -24,6 +29,21 @@ def context():
     }
     return render_template('context.html', **context_dict)
 
+@app.route("/test")
+def text_():
+    text_dict = {
+        "title": "Python Course - Page Test",
+        "str_title": "This is a test page",
+        "str_window":'Цей шаблон я буду використовувати в проекті замість розділу "About".'
+    }
+    return render_template('test.html', **text_dict)
+
 
 if __name__ == '__main__':
     app.run(debug=os.getenv("DEBUG"))
+
+    # {# Это кусок кода, который стал временно не нужен, но удалять жалко
+    #     { % for user in users %}
+    # ...
+    # { % endfor %}
+    # #}
