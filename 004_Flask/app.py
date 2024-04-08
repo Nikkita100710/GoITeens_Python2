d41c0591c851602665a0f56f9ccf9856588cb551
Leonid Kadantsev <leonid.cordon@gmail.com>
2024-04-08 18:27:45 +0300

    CW  (14 11 2023)  -  CRUD

--- 004_Flask/app.py
+++ 004_Flask/app.py
@@ -1,7 +1,7 @@
 import os
 from flask import Flask, render_template, redirect, request
 from dotenv import load_dotenv
-from models import db, Article, User
+from models import db, Article
 from datetime import datetime
 
 load_dotenv()
@@ -39,7 +39,7 @@ def create_article():
         try:
             db.session.add(article)
             db.session.commit()
-            return redirect("/")
+            return redirect("/articles  ")
         except Exception as exc:
             return f"ПРи збереженні запису у базу даних виникла помилка: {exc}"
     else:
@@ -57,6 +57,35 @@ def articles_detail(id):
     article = Article.query.get(id)
     return render_template("articles_detail.html", article=article)
 
+
+@app.route("/articles/<int:id>/delete")
+def articles_delete(id):
+    article = Article.query.get_or_404(id)
+
+    try:
+        db.session.delete(article)
+        db.session.commit()
+        return redirect("/articles")
+    except Exception as exc:
+        return f"При удалении окна ощибка: {exc}"
+
+
+@app.route("/articles/<int:id>/update", methods=["POST", "GET"])
+def articles_update(id):
+    article = Article.query.get(id)
+
+    if request.method == "POST":
+        article.title = request.form["title"]
+        article.intro = request.form["intro"]
+        article.text = request.form["text"]
+
+        try:
+            db.session.commit()
+            return redirect("/articles")
+        except Exception as exc:
+            return f"Про обновлении записи произошла ошибкаЖ {exc}"
+    else:
+        return render_template("article_update.html", article=article)
 
 
 if __name__ == '__main__':
